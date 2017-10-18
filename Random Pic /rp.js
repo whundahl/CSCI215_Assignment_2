60a19a8a1310801bff1f7c5c006b764b7aab482f
Will Hundahl <willhundahl@Will-Hundahls-MacBook-Pro.local>
2017-10-18 13:49:13 -0400

    edits

--- Random Pic /rp.js
+++ Random Pic /rp.js
@@ -11,9 +11,9 @@ var ImageGall =["../images/1.jpg","../images/2.jpg","../images/3.jpg","../images
 
 function displayImage(){
 
-    var counter = Math.floor(Math.random() * (ImageGall.length));
+    var rand = Math.floor(Math.random() * (ImageGall.length));
 
-    document.getElementById("RandomImages").src = myImageGallery[counter];
+    document.getElementById("RandomImages").src = ImageGall[rand];
 
 }
 
